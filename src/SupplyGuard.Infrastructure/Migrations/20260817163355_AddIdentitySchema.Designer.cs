40f9f8c0ec52c76ed71db9690a9bfc5b87af92d5
Fatih Altunel <jjrousseau1128@gmail.com>
2026-08-17 19:40:22 +0300

    chore(infrastructure): generate identity and refresh token schema migration

--- src/SupplyGuard.Infrastructure/Migrations/20260817163355_AddIdentitySchema.Designer.cs
+++ src/SupplyGuard.Infrastructure/Migrations/20260817163355_AddIdentitySchema.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SupplyGuard.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using SupplyGuard.Infrastructure.Persistence;
 namespace SupplyGuard.Infrastructure.Migrations
 {
     [DbContext(typeof(SupplyGuardDbContext))]
-    partial class SupplyGuardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260817163355_AddIdentitySchema")]
+    partial class AddIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
